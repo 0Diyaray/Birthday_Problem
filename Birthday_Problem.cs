d980dfebecf3763f7e1c91f6cb696ee22a58ce37
Diyar Ay <165725225+0Diyaray@users.noreply.github.com>
2024-09-01 18:40:13 +0300

    variables changed to be meaningful

--- Birthday_Problem.cs
+++ Birthday_Problem.cs
@@ -1,40 +1,40 @@
-﻿class Problem()
+﻿class Birthday_Problem()
 {
 	static void Main()
 	{
 		Random random = new Random();
-		List<bool> all = new List<bool>();
+		List<bool> classrooms_status = new List<bool>();
 
-		int classroom_size = 23; // Size of each classrooms
-		double iteration = 10000; // Classroom amount that will be produced for calculation.
+		int classroom_size = 23; // Size of each classroom
+		double iteration = 100000; // Classroom amount that will be produced for calculation.
 
 		for (int i = 0; i < iteration; i++)
 		{
-			all.Add(get_probability(classroom_size));
+			classrooms_status.Add(get_probability(classroom_size));
 		}
 
-		double percup = all.Count(x => x == true);
-		double perdown = all.Count(x => x == false);
+		double percup = classrooms_status.Count(x => x == true);
+		double perdown = classrooms_status.Count(x => x == false);
 		double perc = percup / iteration;
 		Console.WriteLine(perc*100 + "%");
 
 		bool get_probability(int x)
 		{
 			List<int> classroom = new List<int>();
-			bool checkd = false;
-			for (int i = 0; i != x; i++)
+			bool isduplicate = false;
+			for (int person = 0; person != x; person++)
 			{
-				int no = random.Next(1, 366);
-				classroom.Add(no);
+				int dayof_birth = random.Next(1, 366);
+				classroom.Add(dayof_birth);
 			}
-			foreach (int i in classroom)
+			foreach (int persons_birthday in classroom)
 			{
-				if (classroom.Count(x => x == i) > 1 && checkd == false)
+				if (classroom.Count(x => x == persons_birthday) > 1 && isduplicate == false)
 				{
 					return true;
 				}
 			}
-			return checkd;
+			return isduplicate;
 		}
 	}
 }
