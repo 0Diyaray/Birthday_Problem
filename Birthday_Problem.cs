82ba44b58a6dfdfb4004c6f2348fd1a38300f16f
Diyar Ay <165725225+0Diyaray@users.noreply.github.com>
2024-09-01 17:41:58 +0300

    Proje dosyası ekle.

---
+++ Birthday_Problem.cs
@@ -0,0 +1,40 @@
+﻿class Problem()
+{
+	static void Main()
+	{
+		Random random = new Random();
+		List<bool> all = new List<bool>();
+
+		int classroom_size = 23; // Size of each classrooms
+		double iteration = 10000; // Classroom amount that will be produced for calculation.
+
+		for (int i = 0; i < iteration; i++)
+		{
+			all.Add(get_probability(classroom_size));
+		}
+
+		double percup = all.Count(x => x == true);
+		double perdown = all.Count(x => x == false);
+		double perc = percup / iteration;
+		Console.WriteLine(perc*100 + "%");
+
+		bool get_probability(int x)
+		{
+			List<int> classroom = new List<int>();
+			bool checkd = false;
+			for (int i = 0; i != x; i++)
+			{
+				int no = random.Next(1, 366);
+				classroom.Add(no);
+			}
+			foreach (int i in classroom)
+			{
+				if (classroom.Count(x => x == i) > 1 && checkd == false)
+				{
+					return true;
+				}
+			}
+			return checkd;
+		}
+	}
+}
