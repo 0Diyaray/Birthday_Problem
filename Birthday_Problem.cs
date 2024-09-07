fa3ff097939c3400f90cc820d0a30bd7669900de
Diyar Ay <165725225+0Diyaray@users.noreply.github.com>
2024-09-07 15:03:18 +0300

    Added probability calculation for 3 people with the same birthday

--- Birthday_Problem.cs
+++ Birthday_Problem.cs
@@ -1,11 +1,11 @@
-﻿class Birthday_Problem()
+﻿class Birthday_Problem
 {
 	static void Main()
 	{
 		Random random = new Random();
-		List<bool> classrooms_status = new List<bool>();
+		List<(bool,bool)> classrooms_status = new List<(bool, bool)>();
 
-		int classroom_size = 23; // Size of each classroom
+		int classroom_size = 50; // Size of each classroom
 		double iteration = 100000; // Classroom amount that will be produced for calculation.
 
 		for (int i = 0; i < iteration; i++)
@@ -13,15 +13,17 @@
 			classrooms_status.Add(get_probability(classroom_size));
 		}
 
-		double percup = classrooms_status.Count(x => x == true);
-		double perdown = classrooms_status.Count(x => x == false);
-		double perc = percup / iteration;
-		Console.WriteLine("Percentage:" + perc*100 + "%");
-
-		bool get_probability(int size)
+		double classes_duplicate = classrooms_status.Count(x => x.Item1 == true);
+		double classes_triplicate = classrooms_status.Count(x => x.Item2 == true);
+		double perc_duplicate = classes_duplicate / iteration;
+		double perc_triplicate = classes_triplicate / iteration;
+		Console.WriteLine("Duplicate Percentage:" + perc_duplicate*100 + "%");
+		Console.WriteLine("Triplicate Percentage:" + perc_triplicate * 100 + "%");
+		(bool,bool) get_probability(int size)
 		{
 			List<int> classroom = new List<int>();
 			bool isduplicate = false;
+			bool istriplicate = false;
 			for (int person = 0; person != size; person++)
 			{
 				int dayof_birth = random.Next(1, 366);
@@ -31,10 +33,14 @@
 			{
 				if (classroom.Count(x => x == persons_birthday) > 1 && isduplicate == false)
 				{
-					return true;
+					isduplicate=true;
+				}
+				if (classroom.Count(x => x == persons_birthday) > 2 && istriplicate == false)
+				{
+					istriplicate=true;
 				}
 			}
-			return isduplicate;
+			return (isduplicate,istriplicate);
 		}
 	}
 }
